578f768130892cae869d19d929f6785acc076108
JeremyHendr <jeremyhendrikse@gmail.com>
2024-10-02 15:06:54 +0200

    Graph header

---
+++ Header_files/Graph.h
@@ -0,0 +1,30 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <unordered_map>
+#include "Vertex.h"
+#include "Edge.h"
+#include <string>
+
+using std::string;
+
+class Graph {
+    public:
+        Graph(string *gps_data_file);
+
+        void BFS();
+        void Dijkstra();
+        void Astar();
+    
+    private:
+        std::unordered_map<std::int, Vertex > vertices_map;
+        std::unordered_map<std::int, Edge > edge_map;
+}
+
+
+#endif
+
+// this_is_my_int
+// thisIsMyFunction
+// ThisIsMyClass
---
+++ Source_files/Graph.cpp
@@ -0,0 +1,9 @@
+#include "Graph.h"
+#include <string>
+
+using std::string;
+
+
+Graph::Graph(string gps_data_file) {
+    //TODO
+}
