b9ba0e9b354462e39fe4a7a5b03ba0f1cf420df9
JeremyHendr <jeremyhendrikse@gmail.com>
2024-11-06 18:41:15 +0100

    clean

--- Graph_path_finding/Window.h
+++ Graph_path_finding/Window.h
@@ -32,21 +32,3 @@ class MainWindow : public QWidget {
 
 
 
-// #ifndef WINDOW_H
-// #define WINDOW_H
-
-// #include <QGraphicsScene>
-
-// #include "Graph.h"
-
-// class Window : public QGraphicsScene {
-//     Q_OBJECT
-//     public:
-//         Window(QObject  *parent = nullptr );
-//         void displayGraph(Graph* graph);
-
-//     private:
-
-// };
-
-// #endif // WINDOW_H
